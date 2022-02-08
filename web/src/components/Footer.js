8105cdfac66a61417a08e6f9422f0a4d1596780c
Inma Sánchez Casabán <iscasaban@gmail.com>
2022-02-08 15:21:10 +0100

    update images' size & favicon

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -5,7 +5,7 @@ import logoFooter from '../images/logo_footer.png';
 const Footer = (props) => {
   return (
     <footer className='footer'>
-      <h5 className='footerCopy'>Awesome profile-cards &copy; 2021</h5>
+      <h5 className='footerCopy'>PS Management Profile Cards &copy; 2022</h5>
       <img className='imageLogoAdalab' src={logoFooter} alt='logo' title='logo' />
     </footer>
   );
